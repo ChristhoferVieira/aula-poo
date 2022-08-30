e92e3b656dbed372feca87e87ffb32cc3c204284
Christhofer Vieira <christhofer.vieira@alunos.sc.senac.br>
2022-08-29 21:16:48 -0300

    adição de atributos

--- src/poo/POO.java
+++ src/poo/POO.java
@@ -15,7 +15,18 @@ public class POO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Carro meuCarro = new Carro();
+        meuCarro.setCor("preto");
+        meuCarro.setNumPortas(4);
+        meuCarro.setPlaca("QWE-1234");
+        meuCarro.setTipo("Gol");
+        
+        System.out.println("-----------CARRO-----------");
+        System.out.println("Cor: "+meuCarro.getCor());
+        System.out.println("Nº Portas: "+meuCarro.getNumPortas());
+        System.out.println("Placa: "+meuCarro.getPlaca());
+        System.out.println("Tipo: "+meuCarro.getTipo());
+        
     }
     
 }
